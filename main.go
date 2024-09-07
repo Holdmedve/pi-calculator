6f8b198e7ae5a3bfb52ea3c1cb898a8a5431f78b
Holdmedve <talos.boti@gmail.com>
2024-09-07 20:00:54 +0200

    User input

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(CalculatePi())
+	for true {
+		fmt.Println("Type an action:")
+		fmt.Println("Calculate Pi (p):")
+		fmt.Println("Exit (e):")
+
+		var input rune
+		var exit bool = false
+		fmt.Scanf("%c", &input)
+
+		switch(input) {
+			case 'p':
+				fmt.Println(CalculatePi())
+			case 'e':
+				exit = true
+		}
+		
+		if exit {
+			break
+		}
+	}
 }
